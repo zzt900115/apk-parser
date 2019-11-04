398f0ec27e9435aa64421a55e81a6232736b32d1
zhaozhentian <zhaozhentian@didiglobal.com>
2019-11-05 00:37:40 +0800

    增加metadata的读取

--- src/main/java/net/dongliu/apk/parser/Main.java
+++ src/main/java/net/dongliu/apk/parser/Main.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Main method for parser apk
