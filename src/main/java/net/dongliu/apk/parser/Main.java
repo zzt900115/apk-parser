5b1928a8c25228a8032562af13e88e21c4110061
zhaozhentian <zhaozhentian@didiglobal.com>
2019-11-04 17:49:15 +0800

    个人代码提交  生成独立可运行文件

--- src/main/java/net/dongliu/apk/parser/Main.java
+++ src/main/java/net/dongliu/apk/parser/Main.java
@@ -1,5 +1,6 @@
 package net.dongliu.apk.parser;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.util.Locale;
@@ -13,11 +14,23 @@ public class Main {
     public static void main(String[] args) throws IOException, CertificateException {
         String action = args[0];
         String apkPath = args[1];
+
+        if(!new File(apkPath).exists()){
+            System.err.println("！！！！apk文件不存在");
+            System.exit(1);
+        }
+
         try (ApkFile apkFile = new ApkFile(apkPath)) {
             apkFile.setPreferredLocale(Locale.getDefault());
             switch (action) {
                 case "meta":
                     System.out.println(apkFile.getApkMeta());
+                    break;
+                case "versionName":
+                    System.out.println(apkFile.getApkMeta().getVersionName());
+                    break;
+                case "versionCode":
+                    System.out.println(apkFile.getApkMeta().getVersionCode());
                     break;
                 case "manifest":
                     System.out.println(apkFile.getManifestXml());
